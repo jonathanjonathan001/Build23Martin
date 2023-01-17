0b2e7153cd25932b3838de3f484ba9acba236b90
Jonathan Råsmark <jonathanrosemark@gmail.com>
2023-01-17 16:19:10 +0100

    Add test

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+
         System.out.println("Hello There!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Write two numbers:");
+        var a = scanner.nextInt();
+        var b = scanner.nextInt();
+
+        System.out.println(add(a,b));
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
     }
 }
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -2,11 +2,24 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class AppTest {
     @Test
     void test() {
+
+        assertTrue(true);
         assertThat(true).isTrue();
+    }
+
+    @Test
+    void addTwoNumbersReturnsSum() {
+        var result = App.add(1,1);
+
+        assertEquals(2,result);
     }
 }
