04ec1bcc9368cdbcf852d4e205586880f6934b86
Jonathan Råsmark <jonathanrosemark@gmail.com>
2023-01-23 20:49:11 +0100

    Refactor test code for CustomerReader

--- src/test/java/org/example/testdoubles/CustomerReaderTest.java
+++ src/test/java/org/example/testdoubles/CustomerReaderTest.java
@@ -1,6 +1,7 @@
 package org.example.testdoubles;
 
 import org.example.testdoubles.utils.EntityManager;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -8,24 +9,33 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CustomerReaderTest {
 
+    CustomerReader customerReader;
+    EntityManager entityManager;
+
+    @BeforeEach
+    void setUp() {
+        entityManager = Mockito.mock(EntityManager.class);
+        customerReader = new CustomerReader(entityManager);
+    }
+
     @Test
     void findFullNameWithValidIdReturnsFullNameOfCustomer() {
-        EntityManager entityManager = Mockito.mock(EntityManager.class);
+
         Customer customer = new Customer(1,"Kalle", "Anka");
         Mockito.when(entityManager.find(1L)).thenReturn(customer);
-        CustomerReader customerReader = new CustomerReader(entityManager);
 
         String fullName = customerReader.findFullName(1L);
+
         assertEquals("Kalle Anka", fullName);
     }
 
     @Test
     void callingFindFullNameWithInvalidIdReturnsEmptyString(){
-        EntityManager entityManager = Mockito.mock(EntityManager.class);
+
         Mockito.when(entityManager.find(2L)).thenReturn(null);
-        CustomerReader customerReader = new CustomerReader(entityManager);
 
         String fullName = customerReader.findFullName(2L);
+
         assertEquals("", fullName);
     }
     
