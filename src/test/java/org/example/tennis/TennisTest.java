c34911742f69755744f510b62fdf7a707f17109e
Jonathan Råsmark <jonathanrosemark@gmail.com>
2023-01-25 14:05:57 +0100

    Clean imports

--- src/test/java/org/example/tennis/TennisTest.java
+++ src/test/java/org/example/tennis/TennisTest.java
@@ -1,8 +1,6 @@
 package org.example.tennis;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
